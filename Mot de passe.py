cb3ab2237e7516e036e73f36dea3fcba36baca7e
Nolhan_p <plaideaunolhan606@gmail.com>
2026-01-26 09:21:44 +0300

    Add files via upload

---
+++ Mot de passe.py
@@ -0,0 +1,28 @@
+import hashlib
+
+# Demande à l'utilisateur d'entrer un mot de passe
+World = input("Entrez votre mot de passe : ")
+# demande nom du compte
+Account = input("Entrez le nom de votre compte : ")
+# liste pour stocker les mots de passe 
+list = []
+
+def hash_password(password):
+    return hashlib.sha256(password.encode("utf-8")).hexdigest() # mot de passe => hash
+
+def save_password(password, account):
+    hash_value = hash_password(password)
+
+    list.append("account name = " + account + ", " + "Pass world = " + hash_value)
+
+    print("Mot de passe enregistré") # Enregistrement du mot de passe hash
+
+def Enregistrer():
+    resp = str(input("Voulez-vous enregistrer le mot de passe ? (oui/non) : "))
+    if resp == "oui":
+        save_password(World, Account)
+    if resp == "non":
+        print("Mot de passe non enregistré") # Demande d'enregistrement
+
+Enregistrer()
+print(list)
